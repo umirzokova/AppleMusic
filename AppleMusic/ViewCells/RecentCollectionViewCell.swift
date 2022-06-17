801d47b209820cad391bf8e90a9fd56fd93eeb32
Behruz Numonov <bxncorp@gmail.com>
2022-06-17 16:35:13 +0500

    RecentCollectionViewCell

---
+++ AppleMusic/ViewCells/RecentCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecentCollectionViewCell.swift
+//  AppleMusic
+//
+//  Created by Behruzinho on 17.06.2022.
+//
+
+import UIKit
+
+class RecentCollectionViewCell: UICollectionViewCell {
+    
+}
