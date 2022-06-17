ba6a2ad24504775487c499dc2ce119180daa64b5
Nargiz Umirzokova <nargiz.umirzokova@bk.ru>
2022-06-17 15:17:13 +0500

    Added custom color

---
+++ AppleMusic/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  AppleMusic
+//
+//  Created by Nargiz on 17/06/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let skyBlue = UIColor(red: 0.725, green: 1.000, blue: 0.993, alpha: 1)
+}
